491ae0b58c5b3fdfe4d55fdc77d5e3c4c3215bcf
Katie Oneil <>
2021-06-05 23:32:28 +0000

    case insensitive ln26 grade quiz ln14,29,41-48 let know pass/fail ln42-47

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,7 +11,7 @@ let candidateAnswer = ("")
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
-
+let numCorrectAnswers = 0
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -23,9 +23,10 @@ function askQuestion() {
 for (let i = 0; i < questions.length; i++){
   candidateAnswers = (input.question(questions[i]));
 
-    if (candidateAnswers == correctAnswers[i]){
+    if (candidateAnswers.toUpperCase() === correctAnswers[i].toUpperCase()){
     console.log(`Your Answer: ${candidateAnswers}`);
     console.log(`Correct Answer: ${correctAnswers[i]} \n`);
+     numCorrectAnswers++
      }
     else {
     console.log(`I'm sorry that is incorrect. Your answer was ${candidateAnswers}. The correct answer is ${correctAnswers[i]}. \n`);
@@ -37,10 +38,14 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-  let grade;
-  
-
-  return grade;
+  let grade = (numCorrectAnswers/questions.length)*100;
+    if (grade >80){
+      console.log(`Congratulations you have passed!`)
+    }
+    else{
+      console.log(`I'm sorry, you have failed, you need at lease 80% to pass.`)
+    }
+  return console.log(`Score: ${grade}%`);
 }
 
 function runProgram() {
